506d6cab6f8666ce38ef28fc7e7a64244ea07713
dheerajcap <dheerajkumar@ip-192-168-1-5.eu-west-1.compute.internal>
2024-01-21 04:24:24 +0530

    fixed bugs(columns were not having any num)

--- src/main/java/com/onito/tambola/Controller/TicketController.java
+++ src/main/java/com/onito/tambola/Controller/TicketController.java
@@ -9,25 +9,23 @@ import java.util.*;
 public class TicketController {
     @ResponseBody
     @GetMapping("create/{N}")
-    public ArrayList<String> create(@PathVariable int N)
+    public int[][][] create(@PathVariable int N)
     {
-        ArrayList<String> al = new ArrayList<>();
         for(int i=0;i<N;i++)
         {
             LinkedList<LinkedList<Integer>> trackNum=generate90Nums();
-            for(int j=0;j<6;j++)
-            {
-                try {
-                    al.add(generate6(trackNum));
-                }
-                catch (Exception e)
-                {
-                    System.out.println(e);
-                    e.printStackTrace();
-                }
+
+            try {
+                return generate6(trackNum);
             }
+            catch (Exception e)
+            {
+                System.out.println(e);
+                e.printStackTrace();
+            }
+
         }
-        return al;
+        return null;
 
     }
 
@@ -53,77 +51,98 @@ public class TicketController {
         }
         return trackNum;
     }
-    private LinkedList<LinkedList<Integer>> generateCols() {
-        LinkedList<LinkedList<Integer>> trackCol= new LinkedList<>();
-
-        for(int i=0;i<3;i++)
+    private LinkedList<LinkedList<LinkedList<Integer>>> generateCols() {
+        LinkedList<LinkedList<LinkedList<Integer>>> trackCol= new LinkedList<>();
+        for(int k=0;k<6;k++)
         {
-            LinkedList<Integer> nums=new LinkedList<>();
-            for(int j=0;j<9;j++)
+            LinkedList<LinkedList<Integer>> twoD = new LinkedList<>();
+            for (int i = 0; i < 3; i++)
             {
-                nums.add(j);
+                LinkedList<Integer> oneD = new LinkedList<>();
+                for (int j = 0; j < 9; j++)
+                {
+                    oneD.add(j);
+                }
+                twoD.add(oneD);
             }
-
-            trackCol.add(nums);
+            trackCol.add(twoD);
         }
         return trackCol;
     }
 
-    String generate6(LinkedList<LinkedList<Integer>> trackNum)
+    int[][][] generate6(LinkedList<LinkedList<Integer>> trackNum)
     {
-        LinkedList<LinkedList<Integer>> trackCol=generateCols();
+        LinkedList<LinkedList<LinkedList<Integer>>> trackCol=generateCols();
         Random random=new Random();
-        int[][] ticket = new int[3][9];
-        int rowCount[]=new int[3];
+        int[][][] ticket = new int[6][3][9];
+        int rowCount[][]=new int[6][3];
         //making all columns have at least one val
-        for(int col=0;col<9;col++)
+        for(int i=0;i<6;i++)
         {
-            int row = random.nextInt(3);
-            while(rowCount[row]==5)
+            for (int col = 0; col < 9; col++)
             {
-                row = random.nextInt(3);
-            }
-            if(trackNum.get(col).size()>0)
-            {
-                Collections.shuffle(trackNum.get(col));
-                ticket[row][col]=trackNum.get(col).remove(0);
-                trackCol.get(row).remove((Object)col);
-                rowCount[row]++;// remembering which row has how many values.(exactly 5 we need in every row)
-            }
-        }
-        for(int row=0;row<3;row++)
-        {
-            while(rowCount[row]<5 && trackCol.get(row).size()>0)
-            {
-                Collections.shuffle(trackCol.get(row));
-                int col=trackCol.get(row).remove(0);
-                if(trackNum.get(col).size()>0)
+                int row = random.nextInt(3);
+                while (rowCount[i][row] == 5)
+                {
+                    row = random.nextInt(3);
+                }
+                if (trackNum.get(col).size() > 0)
                 {
                     Collections.shuffle(trackNum.get(col));
-                    ticket[row][col]=trackNum.get(col).remove(0);
-                    trackCol.get(row).remove((Object)col);
-                    rowCount[row]++;// remembering which row has how many values.(exactly 5 we need in every row)
+                    ticket[i][row][col] = trackNum.get(col).remove(0);
+                    trackCol.get(i).get(row).remove((Object) col);
+                    rowCount[i][row]++;// remembering which row has how many values.(exactly 5 we need in every row)
                 }
             }
         }
-
-        for(int col=0;col<9;col++)
+        for(int i=0;i<6;i++)
         {
-            int toSort[]=new int[3];
-            for(int row=0;row<3;row++)
+            for (int row = 0; row < 3; row++)
             {
-                toSort[row]=ticket[row][col];
-            }
-            Arrays.sort(toSort);
-            for(int row=0,i=0;row<3;row++)
-            {
-                if(ticket[row][col] != 0) {
-                    while (i < 3 && toSort[i] == 0 && ++i > 0) {}
-                    ticket[row][col] = toSort[i++];
+                while (rowCount[i][row] < 5 && trackCol.get(i).get(row).size() > 0)
+                {
+                    Collections.shuffle(trackCol.get(i).get(row));
+                    int col = trackCol.get(i).get(row).remove(0);
+                    if (trackNum.get(col).size() > 0)
+                    {
+                        Collections.shuffle(trackNum.get(col));
+                        ticket[i][row][col] = trackNum.get(col).remove(0);
+                        trackCol.get(row).remove((Object) col);
+                        rowCount[i][row]++;// remembering which row has how many values.(exactly 5 we need in every row)
+                    }
                 }
             }
         }
-        return String.format("%s\n%s\n%s\n",Arrays.toString(ticket[0]),Arrays.toString(ticket[1]),Arrays.toString(ticket[2]));
+        for(int i=0;i<6;i++)
+        {
+            for(int col=0;col<9;col++)
+            {
+                int toSort[]=new int[3];
+                for(int row=0;row<3;row++)
+                {
+                    toSort[row]=ticket[i][row][col];
+                }
+                Arrays.sort(toSort);
+                for(int row=0,h=0;row<3;row++)
+                {
+                    if(ticket[i][row][col] != 0) {
+                        while (h < 3 && toSort[h] == 0 && ++h > 0) {}
+                        ticket[i][row][col] = toSort[h++];
+                    }
+                }
+            }
+        }
+        for(int i=0;i<6;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                System.out.println(Arrays.toString(ticket[i][j]));
+            }
+            System.out.println();
+            System.out.println();
+        }
+        return ticket;
     }
+
 
 }
